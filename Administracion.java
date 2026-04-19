c5f675d73dde7cf48e923300f5b1f45708d7faba
Lucas Ruiz <l.ruiz11@uniandes.edu.co>
2026-04-19 14:58:15 -0500

    prints mas bacanos

--- Administracion.java
+++ Administracion.java
@@ -15,6 +15,7 @@ public class Administracion extends Thread {
                 Thread.yield();
             }
             if (e.esFin()) {
+                System.out.println("Se efectuan "+ nc + " eventos de fin para los clasificadores");
                 for (int i = 0; i < nc; i++) {
                     while (!buzonClasificacion.depositar(new Evento(true))) {
                         Thread.yield();
@@ -23,10 +24,12 @@ public class Administracion extends Thread {
                 break;
             }
             int aleatorio = (int)(Math.random() * 21);
+
             // la coneccion con el buzon de clasificacion es semiactiva por lo que usa esta manera de depositar
             if (aleatorio % 4 == 0) {
                 while (!buzonClasificacion.depositar(e)) {
                     Thread.yield();
+                    System.out.println("infonsivo el evento: "+ e.toString());
                 }
             }
         }
